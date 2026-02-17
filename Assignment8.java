8c5feb538ca7737eeb021bdc0083cdc6623cfb8f
Moses Hlohlela <141419939+MosheHlohlela@users.noreply.github.com>
2026-02-17 18:46:36 +0200

    Add files via upload

---
+++ Assignment8.java
@@ -0,0 +1,26 @@
+package Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment8 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("autocomplete")).sendKeys("Sou");
+		Thread.sleep(1200);
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+		System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value")); 
+
+		
+	}
+
+	
+	
+}
